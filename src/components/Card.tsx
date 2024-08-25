df6cc6d08e1fd3a05587741f7e761adb9de511a7
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-25 18:41:10 +0100

    update card component, add animation to the projects section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,15 @@
-import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
 
-const Card = ({ children, className }: PropsWithChildren<{ className?: string }>) => {
+const Card = ({ children, className, ...other }: ComponentPropsWithoutRef<'div'>) => {
     return (
-        <div className={twMerge("bg-gray-800 rounded-3xl relative overflow-hidden z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}>
+        <div
+            className={twMerge("bg-gray-800 rounded-3xl relative overflow-hidden z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}
+            {...other}
+        >
             <div className="absolute inset-0 -z-10 opacity-5" style={{ backgroundImage: `url(${grainImage.src})` }
             }></div>
             {children}
